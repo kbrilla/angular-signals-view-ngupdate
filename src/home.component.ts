a6c700ffada234c52776df2a618282093ef4b39e
kbrilla <criskrzysiu@gmail.com>
2023-04-06 11:39:13 +0200

    2 files modified

--- src/home.component.ts
+++ src/home.component.ts
@@ -5,7 +5,6 @@ import {
   signal,
   OnChanges,
   SimpleChanges,
-  ChangeDetectionStrategy,
 } from '@angular/core';
 
 @Component({
@@ -13,9 +12,7 @@ import {
   standalone: true,
   imports: [CommonModule],
   template: `
-  <pre>
-    {{query() | json}}
-  </pre>
+    {{query().id}}
   `,
 })
 export class HomeComponent implements OnChanges {
@@ -27,37 +24,6 @@ export class HomeComponent implements OnChanges {
       console.log(changes.id);
       this.query.set({
         id: changes.id.currentValue,
-      });
-    }
-  }
-}
-
-@Component({
-  selector: 'ng-homez',
-  standalone: true,
-  imports: [CommonModule],
-  template: `
-  <pre>
-    {{querys() | json}}
-    {{querys2() | json}}
-
-  </pre>
-  `,
-})
-export class HomezComponent implements OnChanges {
-  querys = signal({ id: 1 });
-  querys2 = signal({ id: 1 });
-
-  @Input() id2: number;
-
-  ngOnChanges(changes: SimpleChanges): void {
-    if (changes.id2) {
-      console.log(changes.id2);
-      this.querys.set({
-        id: changes.id2.currentValue,
-      });
-      this.querys2.set({
-        id: changes.id2.currentValue,
       });
     }
   }
--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,42 @@
 import 'zone.js/dist/zone';
-import {
-  ChangeDetectionStrategy,
-  Component,
-  enableProdMode,
-} from '@angular/core';
+import { Component, enableProdMode } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { HomeComponent, HomezComponent } from './home.component';
+import { HomeComponent } from './home.component';
 
 enableProdMode();
 
 @Component({
   selector: 'my-app',
   standalone: true,
-  imports: [CommonModule, HomeComponent, HomezComponent],
+  imports: [CommonModule, HomeComponent],
   template: `
-    <ng-container *ngIf="true">
-    <ng-home  [id]="3" />
-    <ng-homez  [id2]="3" />
-    </ng-container>
+  <ng-container *ngIf="true">
+<p>
+  <ng-home [id]="6" /> 
+  <ng-home [id]="5" />
+</p>
+<p>
+//incorect should be 6 5
+</p>
+  </ng-container>
+  <p>
+  <ng-home *ngIf="true" [id]="6" /> 
+  <ng-home *ngIf="true" [id]="5" />
+  </p>
+  <p>
+  correct 6 5 cause ngIf wrapping individually
+  </p>
+  <ng-container *ngIf="true">
+  <p>
+  <ng-home [id]="6" /> 
+  <ng-home [id]="5" />
+  <ng-home [id]="4" />
+  </p>
+  <p>
+  here shoudl be 6 5 4 but completly broken cause 3x used in single view
+  </p>
+  </ng-container>
   `,
 })
 export class App {
